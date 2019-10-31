f52c74c65bd9fc420f708c409d9ddf409dc572ed
Pavel Subach <psubach@rbc.ru>
2019-10-31 15:55:29 +0300

    Test for handler

--- Sources/MicroExpress/Express.swift
+++ Sources/MicroExpress/Express.swift
@@ -5,92 +5,100 @@ import NIO
 import NIOHTTP1
 
 enum BindableHost {
-	case localhost
-	case any
-	case ipv4(String)
+    case localhost
+    case any
+    case ipv4(String)
 }
 
 extension BindableHost {
-	var host: String {
-		switch self {
-		case .localhost:
-			return "localhost"
-		case .any:
-			// https://en.wikipedia.org/wiki/0.0.0.0
-			return "0.0.0.0"
-		case let .ipv4(ip):
-			return ip
-		}
-	}
+    var host: String {
+        switch self {
+        case .localhost:
+            return "localhost"
+        case .any:
+            // https://en.wikipedia.org/wiki/0.0.0.0
+            return "0.0.0.0"
+        case let .ipv4(ip):
+            return ip
+        }
+    }
 }
 
 open class Express : Router {
-  
-  override public init() {}
-  
-  let loopGroup =
-		MultiThreadedEventLoopGroup(numberOfThreads: System.coreCount)
-  
-  open func listen(_ port: Int) {
-    let reuseAddrOpt = ChannelOptions.socket(
-      SocketOptionLevel(SOL_SOCKET),
-      SO_REUSEADDR)
-    let bootstrap = ServerBootstrap(group: loopGroup)
-      .serverChannelOption(ChannelOptions.backlog, value: 256)
-      .serverChannelOption(reuseAddrOpt, value: 1)
-      .childChannelInitializer { channel in
-				channel.pipeline.addHandler(HTTPServerPipelineHandler()).always {_ in
-					_ = channel.pipeline.addHandler(HTTPHandler(router: self))
+
+    override public init() {}
+
+    let loopGroup =
+        MultiThreadedEventLoopGroup(numberOfThreads: System.coreCount)
+
+    open func listen(_ port: Int) {
+        let reuseAddrOpt = ChannelOptions.socket(
+            SocketOptionLevel(SOL_SOCKET),
+            SO_REUSEADDR)
+        let bootstrap = ServerBootstrap(group: loopGroup)
+            .serverChannelOption(ChannelOptions.backlog, value: 256)
+            .serverChannelOption(reuseAddrOpt, value: 1)
+            .childChannelInitializer { channel in
+                channel.pipeline.addHTTPClientHandlers().always { (result) in
+                    switch result {
+                    case .success:
+                        print("success")
+                    case let .failure(error):
+                        print("error: \(error.localizedDescription)")
+                    }
+                }
+//                channel.pipeline.addHandler(HTTPServerPipelineHandler()).always {_ in
+//                    _ = channel.pipeline.addHandler(HTTPHandler(router: self))
+//                }
         }
-      }
-      
-      .childChannelOption(ChannelOptions.socket(
-        IPPROTO_TCP, TCP_NODELAY), value: 1)
-      .childChannelOption(reuseAddrOpt, value: 1)
-      .childChannelOption(ChannelOptions.maxMessagesPerRead,
-                          value: 1)
-    
-    do {
-      let serverChannel =
-        try bootstrap.bind(host: "localhost", port: port)
-          .wait()
-      print("Server running on:", serverChannel.localAddress!)
-      
-      try serverChannel.closeFuture.wait() // runs forever
-    }
-    catch {
-      fatalError("failed to start server: \(error)")
-    }
-  }
 
-  final class HTTPHandler : ChannelInboundHandler {
-    typealias InboundIn = HTTPServerRequestPart
-    
-    let router : Router
-    
-    init(router: Router) {
-      self.router = router
+        .childChannelOption(ChannelOptions.socket(
+            IPPROTO_TCP, TCP_NODELAY), value: 1)
+            .childChannelOption(reuseAddrOpt, value: 1)
+            .childChannelOption(ChannelOptions.maxMessagesPerRead,
+                                value: 1)
+
+        do {
+            let serverChannel =
+                try bootstrap.bind(host: "localhost", port: port)
+                    .wait()
+            print("Server running on:", serverChannel.localAddress!)
+
+            try serverChannel.closeFuture.wait() // runs forever
+        }
+        catch {
+            fatalError("failed to start server: \(error)")
+        }
     }
 
-    func channelRead(ctx: ChannelHandlerContext, data: NIOAny) {
-      let reqPart = self.unwrapInboundIn(data)
-      
-      switch reqPart {
-        case .head(let header):
-          let req = IncomingMessage(header: header)
-          let res = ServerResponse(channel: ctx.channel)
-          
-          // trigger Router
-          router.handle(request: req, response: res) {
-            (items : Any...) in // the final handler
-            res.status = .notFound
-            res.send("No middleware handled the request!")
-          }
+    final class HTTPHandler : ChannelInboundHandler {
+        typealias InboundIn = HTTPServerRequestPart
 
-        // ignore incoming content to keep it micro :-)
-        case .body, .end: break
-      }
+        let router : Router
+
+        init(router: Router) {
+            self.router = router
+        }
+
+        func channelRead(ctx: ChannelHandlerContext, data: NIOAny) {
+            let reqPart = self.unwrapInboundIn(data)
+
+            switch reqPart {
+            case .head(let header):
+                let req = IncomingMessage(header: header)
+                let res = ServerResponse(channel: ctx.channel)
+
+                // trigger Router
+                router.handle(request: req, response: res) {
+                    (items : Any...) in // the final handler
+                    res.status = .notFound
+                    res.send("No middleware handled the request!")
+                }
+
+            // ignore incoming content to keep it micro :-)
+            case .body, .end: break
+            }
+        }
     }
-  }
 }
 
